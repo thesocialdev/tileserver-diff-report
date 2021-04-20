8a1a5f9529e66cf2d7486170c574efe6bee05594
mbsantos <msantos@wikimedia.org>
2021-04-20 15:51:20 +0200

    Implement WebGL rendering of tiles and enable comparison between raster and MVT rendered with maplibre-gl

--- index.js
+++ index.js
@@ -4,18 +4,31 @@ const TileSample = require('./lib/TileSample')
 
 const options = {
     sample: {
-        startZoom: 3,//9/305/208
-        startX: 4,
-        startY: 4,
-        execFromZoom:12,
-        endZoom: 13,
+        startZoom: 6,//4/11/6
+        startX: 35,
+        startY: 23,
+        execFromZoom: 6,
+        endZoom: 7,
         maxSizePercentage: 5,
-        minSize: 150,  
+        minSize: 150,
     },
     benchmark: {
         servers: [
-            { host: 'http://localhost:6533/osm-intl', label: 'imposm3'},
-            { host: 'http://localhost:6534/osm-intl', label: 'osm2pgsql'},
+            {
+                type: "vector",
+                style: '../osm-bright.json',
+                label: 'OpenMapTiles',
+                params: {
+                    width: 512,
+                    height: 512,
+                    format: 'png'
+                }
+            },
+            {
+                type: "raster",
+                host: 'https://maps.wikimedia.org/osm',
+                label: 'Kartotherian'
+            },
         ],
         threshold: 0.1,
     }
@@ -25,20 +38,20 @@ let sample = new TileSample(options.sample);
 let benchmark = new Benchmark(sample, options.benchmark);
 
 [ "en", "pt", "zh", "ru", "cr"].forEach(async (lang) => {
-    
+
 });
 
 console.log("Executing for language en");
 benchmark.exec("en").then(() => {
-    console.log("Executing for language pt");
-    benchmark.exec("pt").then(() => {
-        console.log("Executing for language zh");
-        benchmark.exec("zh").then(() => {
-            console.log("Executing for language ru");
-            benchmark.exec("ru").then(() => {
-                console.log("Executing for language cr");
-                benchmark.exec("cr");
-            });
-        });
-    });
+    // console.log("Executing for language pt");
+    // benchmark.exec("pt").then(() => {
+    //     console.log("Executing for language zh");
+    //     benchmark.exec("zh").then(() => {
+    //         console.log("Executing for language ru");
+    //         benchmark.exec("ru").then(() => {
+    //             console.log("Executing for language cr");
+    //             benchmark.exec("cr");
+    //         });
+    //     });
+    // });
 });
